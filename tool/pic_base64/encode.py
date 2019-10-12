95dfef8ce6cd2bde9c3577a922afc741ccc4109e
nowmore <9102zyx@gmail.com>
2019-10-12 20:42:19 +0800

    添加图片base64

---
+++ tool/pic_base64/encode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import base64
+from sys import argv
+
+
+def Usage(name):
+    print("Usage python %s path_to_picture" % name)
+    exit()
+    
+if __name__ == "__main__":
+    if len(argv) < 2:
+        Usage(argv[0])
+
+    with open(argv[1], 'rb') as pic:
+        data = base64.b64encode(pic.read())
+        print('data:image/jpeg;base,%s' % data.decode())
